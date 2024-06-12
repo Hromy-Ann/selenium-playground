0d4ac85d917b9ca0d987634f6c30f756e0114261
Anna Hromova <hromovaannaa@gmail.com>
2024-06-13 00:53:08 +0300

    doing homework 15

--- src/test/java/com/demoqa/MyFirstUITest.java
+++ src/test/java/com/demoqa/MyFirstUITest.java
@@ -5,21 +5,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class MyFirstUITest {
     private static final String FORMS_CARD_TEXT = "//div[@class='card-body']/h5[text()='Forms']";
     private static final String ELEMENTS_CARD_TEXT = "//div[@class='card-body']/h5[text()='Elements']";
+    private static final String WIDGETS_CARD_TEXT = "//div[@class='card-body']/h5[text()='Widgets']";
     private static final String SPAN_TEXT_RADIO_BUTTON = "//span[text()='Radio Button']";
     private static final String SPAN_TEXT_TEXT_BOX = "//span[text()='Text Box']";
     private static final String SPAN_TEXT_BUTTONS = "//span[text()='Buttons']";
+    private static final String SPAN_TEXT_PROGRESSBAR = "//span[text()='Progress Bar']";
+    private static final String SPAN_TEXT_TOOL_TIPS = "//span[text()='Tool Tips']";
+    private static final String SPAN_TEXT_TABS = "//span[text()='Tabs']";
     private static final String BUTTON_TEXT_CLICK_ME = "//button[text()='Click Me']";
+    private static final String LABEL_FOR_YES_RADIO = "//label[@for='yesRadio']";
     private static final String SCROLL_INTO_VIEW_SCRIPT = "arguments[0].scrollIntoView(true)";
-    public static final String LABEL_FOR_YES_RADIO = "//label[@for='yesRadio']";
+    private static final String MOUSE_OVER_SCRIPT = "arguments[0].dispatchEvent(" +
+            "new MouseEvent('mouseover', {'view': window, 'bubbles': true, 'cancelable': true}));";
     WebDriver driver;
     JavascriptExecutor js;
 
@@ -93,6 +102,53 @@ class MyFirstUITest {
         clickMeButton.click();
         WebElement result = driver.findElement(By.id("dynamicClickMessage"));
         assertEquals("You have done a dynamic click", result.getText());
+    }
+
+    @Test
+    void clickProgressBar() {
+        WebElement widgetsCard = driver.findElement(By.xpath(WIDGETS_CARD_TEXT));
+        js.executeScript(SCROLL_INTO_VIEW_SCRIPT, widgetsCard);
+        widgetsCard.click();
+        WebElement progressBarMenuItem = driver.findElement(By.xpath(SPAN_TEXT_PROGRESSBAR));
+        progressBarMenuItem.click();
+        WebElement start = driver.findElement(By.id("startStopButton"));
+        js.executeScript(SCROLL_INTO_VIEW_SCRIPT, start);
+        start.click();
+        WebElement progressBarContainer = driver.findElement(By.id("progressBar"));
+        WebElement progressBarValue = progressBarContainer.findElement(By.tagName("div"));
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(10L));
+        wait.until(d -> progressBarValue.getAttribute("aria-valuenow").equals("100"));
+        WebElement resetButton = driver.findElement(By.id("resetButton"));
+        assertTrue(resetButton.isDisplayed());
+    }
+
+    @Test
+    void hoverTheButton() {
+        WebElement widgetsCard = driver.findElement(By.xpath(WIDGETS_CARD_TEXT));
+        js.executeScript(SCROLL_INTO_VIEW_SCRIPT, widgetsCard);
+        widgetsCard.click();
+        WebElement toolTipsMenuItem = driver.findElement(By.xpath(SPAN_TEXT_TOOL_TIPS));
+        toolTipsMenuItem.click();
+        WebElement toolTipButton = driver.findElement(By.id("toolTipButton"));
+        js.executeScript(MOUSE_OVER_SCRIPT, toolTipButton);
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(1L));
+        wait.until(ExpectedConditions.attributeToBe(toolTipButton, "aria-describedby", "buttonToolTip"));
+        WebElement buttonToolTip = driver.findElement(By.id("buttonToolTip"));
+        assertTrue(buttonToolTip.isDisplayed());
+    }
+
+    @Test
+    void chooseTab() {
+        WebElement widgetsCard = driver.findElement(By.xpath(WIDGETS_CARD_TEXT));
+        js.executeScript(SCROLL_INTO_VIEW_SCRIPT, widgetsCard);
+        widgetsCard.click();
+        WebElement tabsMenuItem = driver.findElement(By.xpath(SPAN_TEXT_TABS));
+        tabsMenuItem.click();
+        WebElement tabUse = driver.findElement(By.id("demo-tab-use"));
+        tabUse.click();
+        WebElement tabPaneUse = driver.findElement(By.id("demo-tabpane-use"));
+        String hidden = tabPaneUse.getAttribute("aria-hidden");
+        assertFalse(Boolean.parseBoolean(hidden));
     }
 
     @BeforeEach
